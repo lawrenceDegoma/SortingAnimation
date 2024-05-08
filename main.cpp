5add03b50817ea32c424b469d50b8c1db193ac4a
Law <lawrencedegoma@yahoo.com>
2024-05-08 02:04:35 -0700

    Animation works now

--- main.cpp
+++ main.cpp
@@ -3,42 +3,52 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
-//#include "Application.h"
 #include <SFML/Graphics.hpp>
 
-//void print(int [], int);
 
 const int ARRAY_SIZE = 100;
 const int WINDOW_WIDTH =  800;
 const int WINDOW_HEIGHT = 600;
 
 void quickSort(std::vector<int>&, int, int, sf::RenderWindow&);
+void drawArray(const std::vector<int>&, sf::RenderWindow&);
 
 int main() {
     std::vector<int> nums(ARRAY_SIZE);
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Quick Sort");
 
     srand(time(0));
-    for (int i=0; i< ARRAY_SIZE; ++i){
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
         nums[i] = rand() % WINDOW_HEIGHT + 1;
     }
 
-    quickSort(nums, 0, ARRAY_SIZE -1, window);
+    drawArray(nums, window);
 
-    while (window.isOpen()){
+    bool clicked = false;
+    while (!clicked && window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)){
-            if (event.type == sf::Event::Closed){
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            } else if (event.type == sf::Event::MouseButtonPressed) {
+                clicked = true;
+            }
+        }
+    }
+
+    if (clicked) {
+        quickSort(nums, 0, ARRAY_SIZE - 1, window);
+    }
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
     }
-//    Application app(ARRAY_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT, "Quick Sort");
-//    app.run();
     return 0;
-
-
-
 }
 
 void quickSort(std::vector<int>& nums, int left, int right, sf::RenderWindow& window){
@@ -51,36 +61,41 @@ void quickSort(std::vector<int>& nums, int left, int right, sf::RenderWindow& wi
             do{
                 i++;
             }
-            while (nums[i] < pivot && i <= right);
+            while (i <= right && nums[i] < pivot);
 
             do{
                 j--;
             }
-            while (nums[j] > pivot);
+            while (j >= left && nums[j] > pivot);
 
             if (i >= j)
                 break;
 
             std::swap(nums[i], nums[j]);
+            drawArray(nums, window);
 
-
-            window.clear(sf::Color::White);
-            for (int k=0; k<ARRAY_SIZE; ++k){
-                sf::RectangleShape rect(sf::Vector2f(WINDOW_WIDTH / ARRAY_SIZE, nums[k]));
-                rect.setPosition(k * (WINDOW_WIDTH / ARRAY_SIZE), (WINDOW_HEIGHT - nums[k]));
-
-                rect.setFillColor(sf::Color::Magenta);
-                rect.setOutlineColor(sf::Color::Black);
-                rect.setOutlineThickness(1);
-                window.draw(rect);
-//                sf::sleep(sf::microseconds(10000));
-            }
-            window.display();
+            sf::sleep(sf::milliseconds(50));
         }
 
         std::swap(nums[left], nums[j]);
+        drawArray(nums, window);
+
+        // Introduce a delay (adjust as needed)
+        sf::sleep(sf::milliseconds(50));
 
         quickSort(nums, left, j-1, window);
         quickSort(nums, j+1, right, window);
     }
+}
+
+void drawArray(const std::vector<int>& nums, sf::RenderWindow& window) {
+    window.clear();
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        sf::RectangleShape rect(sf::Vector2f(WINDOW_WIDTH / ARRAY_SIZE, nums[i]));
+        rect.setPosition((i * (WINDOW_WIDTH / ARRAY_SIZE)), WINDOW_HEIGHT - nums[i]);
+
+        rect.setFillColor(sf::Color::White);
+        window.draw(rect);
+    }
+    window.display();
 }
